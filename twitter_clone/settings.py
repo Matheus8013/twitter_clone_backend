32a272e07a9a389e5e79a367283cd83df19c32f9
Matheus Ferreira <matheusdossantosferreira313@gmail.com>
2025-09-10 20:02:31 -0300

    Consertando um erro

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^!w_rvk_uo-iv_4+(e#dt2xvgb##h5)5v@7&n_6ebl@_!g-$qz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
